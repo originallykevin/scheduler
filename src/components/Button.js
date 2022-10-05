9dd26fb9de1713ad969fe18094984889822f4b46
Kevin Ly <ly1@ualberta.ca>
2022-10-04 20:13:02 -0600

    add functional button component and refactor with classNames

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,28 @@
-import React from "react";
-
+import React, { useDeferredValue } from "react";
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  // let buttonClass = "button";
+
+  // if (props.confirm) {
+  //   classNames('buttonClass', {'button--confirm': true});
+  // }
+  // // button button--confirm
+
+  // if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
+
+  const myButtonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return <button
+    className={myButtonClass}
+    onClick={props.onClick}
+    disabled={props.disabled}
+  >{props.children}
+  </button>;
 }

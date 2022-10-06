45d5f1722f26f5be1a3144685e92f53e4fa1eb2a
Kevin Ly <ly1@ualberta.ca>
2022-10-05 23:35:54 -0600

    add initial Edit and Create to stories

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -187,4 +188,20 @@ storiesOf("Appointment", module)
       message="Could not delete appointment"
       onClose={action("onClose")}
     />
-  ));
+  ))
+  .add("Edit", () => {
+    <Form
+      student="Nori"
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  })
+  .add("Create", () => {
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  })

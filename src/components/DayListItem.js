677adab3f8a018808513d74138424a3ace19491a
Kevin Ly <ly1@ualberta.ca>
2022-10-05 19:44:11 -0600

    refactor variable names

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -19,10 +19,12 @@ export default function DayListItem(props) {
 
   return (
     <li onClick={() => props.setDay(props.name)}
+      selected={props.selected}
       className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-
-      {formatSpots(props.spots)}
+      <h3 className="text--light">
+        {formatSpots(props.spots)}
+      </h3>
     </li>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -65,13 +65,13 @@ storiesOf("DayList", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
   .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Monday"} setDay={action("setDay")} />
   ))
   .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Tuesday"} setDay={action("setDay")} />
   ))
   .add("Wednesday", () => (
-    <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Wednesday"} setDay={action("setDay")} />
   ));
 
 // InterviewerListItem
@@ -133,12 +133,12 @@ storiesOf("InterviewerList", module)
   .add("Selected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
+      value={3}
     />
   ))
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ));

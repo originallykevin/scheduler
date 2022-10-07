715a0ea5b0d8ff066bccb8d0c2511a2dd658c8cc
Kevin Ly <ly1@ualberta.ca>
2022-10-07 14:55:58 -0600

    fetch interviewer data, add getInterview help and test

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,47 +6,6 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
-
 export default function Application(props) {
   const setDay = day => setState({ ...state, day });
   // const setDays = (days) => setState(prev => ({ ...prev, days }));
@@ -55,8 +14,6 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
     Promise.all([
@@ -73,12 +30,18 @@ export default function Application(props) {
         }));
       });
   }, []);
+  // console.log(state.interviewers)
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const AppointmentsArray = dailyAppointments.map((appointment) => {
-    return (< Appointment
-      keys={appointment.id}
-      {...appointment}
-    />
+    const interview = getInterview(state, appointment.interview);
+    return (
+      < Appointment
+        keys={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
     );
   });
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,7 +4,7 @@ export function getAppointmentsForDay(state, day) {
   if (state.days.length === 0) {
     return [];
   }
-  
+
   // obj for stats.days
   const dayObj = state.days.find(d => d.name === day);
   // returns an empty array when the day is not found
@@ -13,4 +13,21 @@ export function getAppointmentsForDay(state, day) {
   }
 
   return dayObj.appointments.map(appointmentid => state.appointments[appointmentid]);
+}
+
+
+export function getInterview(state, interview) {
+
+  // returns null if no interview is booked
+  if (!interview) {
+    return null;
+  }
+  // student name
+  const student = interview.student
+  // console.log(student)
+  // specific interviewer 
+  const interviewer = state.interviewers[interview.interviewer]
+  // console.log(interviewer)
+
+  return {student, interviewer}
 }

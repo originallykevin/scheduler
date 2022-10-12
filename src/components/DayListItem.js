f7b4cfffd9da0ed7d4b092c4ab4817e539bdd7b8
Kevin Ly <ly1@ualberta.ca>
2022-10-12 17:57:18 -0600

    add cypress

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -18,13 +18,14 @@ export default function DayListItem(props) {
   });
 
   return (
-    <li data-testid="day" onClick={() => props.setDay(props.name)}
-      selected={props.selected}
-      className={dayClass}>
+    <li
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}
+    selected={props.selected}
+    data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">
-        {formatSpots(props.spots)}
-      </h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
